e39618365a65016c6eeccfa25915a75a6477f8b5
KQraze <pavel.eliseev061@gmail.com>
2025-01-20 14:58:53 +0700

    first commit

---
+++ app/Http/Requests/ApiRequest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+class ApiRequest extends FormRequest
+{
+}
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+/**
+ * POST /authorization
+ * POST /registration
+ * GET /logout
+ * POST /files
+ * PATCH /files/{file_id}
+ * DELETE /files/{file_id}
+ * GET /files/{file_id}
+ * POST /files/{file_id}/accesses
+ * DELETE /files/{file_id}/accesses
+ * GET /files/disk
+ * GET /files/shared
+ */
